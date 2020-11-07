95183c7856442806190c3070c04bf18c35bfdea9
Madhumitha Nara <madhumithanara.171it125@nitk.edu.in>
2020-11-07 20:39:45 +0530

    Added Get Text functionality

--- chrome-extension/src/bg.js
+++ chrome-extension/src/bg.js
@@ -33,3 +33,22 @@ function summarizeText(text) {
 function factCheckText(text) {
     console.log("factCheckText: " + text);
 }
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    console.log('Injecting content script(s)');
+    //On Firefox document.body.textContent is probably more appropriate
+    chrome.tabs.executeScript(tab.id,{
+        code: 'document.body.innerText;'
+        //If you had something somewhat more complex you can use an IIFE:
+        //code: '(function (){return document.body.innerText})();'
+        //If your code was complex, you should store it in a
+        // separate .js file, which you inject with the file: property.
+    },receiveText);
+});
+
+//tabs.executeScript() returns the results of the executed script
+//  in an array of results, one entry per frame in which the script
+//  was injected.
+function receiveText(resultsArray){
+    console.log(resultsArray[0]);
+}
